d4607eb4694281e708abc28f9a3746aab1ec98d4
walals <Lukas.Wala@uk-erlangen.de>
2025-04-16 15:05:46 +0200

    Add localStorage prototyping

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ let date = new Date();
 let timer = document.getElementById("timestampParagraph");
 
 timer.innerText = date.toLocaleDateString();
+
+localStorage.setItem("testString", "25");
+
+console.log(localStorage.getItem("testString"));
--- pages/standort/gebäude/gebäude.js
+++ pages/standort/gebäude/gebäude.js
@@ -3,7 +3,24 @@ function renderStockwerk(stockwerk) {
     // TODO: Räume individuell an Stockwerk anpassen
 }
 
+function raumBuchen(button) {
+    if (localStorage.getItem(button) == null) {
+        localStorage.setItem(button, "gebucht");
+    }
+}
+
 let date = new Date();
 let timer = document.getElementById("timestampParagraph");
+
+let raum101 = document.getElementById("raum1Status");
+
+console.log(raum101.id);
+
+if (localStorage.getItem(raum101.id) === "gebucht") {
+    console.log("bdsfha");
+    raum101.innerHTML = "<p>gebucht</p>";
+}
+
+// raum101.innerHTML = localStorage.getItem("testString");
 
 timer.innerText = date.toLocaleDateString();
