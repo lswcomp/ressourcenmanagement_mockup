4f6df8488d9db987bcf829057e102d7a58dce16c
walals <Lukas.Wala@uk-erlangen.de>
2025-04-16 15:29:06 +0200

    Finish prototype for localStorage Data Entry

--- pages/standort/gebäude/gebäude.js
+++ pages/standort/gebäude/gebäude.js
@@ -6,6 +6,14 @@ function renderStockwerk(stockwerk) {
 function raumBuchen(button) {
     if (localStorage.getItem(button) == null) {
         localStorage.setItem(button, "gebucht");
+        raum101.innerHTML = '<p>gebucht</p><button id="buttonRaum1" onclick="buchungEntfernen(this.id)">Buchung entfernen</button>';
+    }
+}
+
+function buchungEntfernen(button) {
+    if (localStorage.getItem(button) != null) {
+        localStorage.removeItem(button);
+        raum101.innerHTML = '<button id="buttonRaum1" onclick="raumBuchen(this.id)">Buchen</button>';
     }
 }
 
@@ -17,8 +25,7 @@ let raum101 = document.getElementById("raum1Status");
 console.log(raum101.id);
 
 if (localStorage.getItem(raum101.children[0].id) === "gebucht") {
-    console.log("bdsfha");
-    raum101.innerHTML = "<p>gebucht</p>";
+    raum101.innerHTML = '<p>gebucht</p><button id="buttonRaum1" onclick="buchungEntfernen(this.id)">Buchung entfernen</button>';
 }
 
 // raum101.innerHTML = localStorage.getItem("testString");
