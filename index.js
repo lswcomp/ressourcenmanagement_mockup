dc080a16a836c91f2f3b3135c2f08c85b0b35d11
walals <Lukas.Wala@uk-erlangen.de>
2025-06-02 17:34:42 +0200

    Startseite und IndexedDB

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-let date = new Date();
-let timer = document.getElementById("timestampParagraph");
+const indexedDB = window.indexedDB;
 
-timer.innerText = date.toLocaleDateString();
+const request = indexedDB.open("Buchungen", 1);
 
-localStorage.setItem("testString", "25");
+request.onerror = function (event) {
+    console.error(event);
+};
 
-console.log(localStorage.getItem("testString"));
+request.onupgradeneeded = function () {
+    const db = request.result;
+    const store = db.createObjectStore("buchungen", { keyPath: "id" });
+    store.createIndex("item", ["item"], { unique: false });
+    store.createIndex("tag", ["tag"], { unique: false });
+    store.createIndex("zeitstempel", ["zeitstempel"], { unique: true });
+};
+
+request.onsuccess = function () {
+    const db = request.result;
+    const transaction = db.transaction("buchungen", "readwrite");
+
+    const store = transaction.objectStore("buchungen");
+    const itemIndex = store.index("item");
+
+    store.put({ item: "Tisch 1", tag: "02.06.2025", zeitstempel: "17:26:00"});
+
+    const query = store.getAll(itemIndex);
+
+    console.log(query.result);
+}
+
+function toggleMenu() {
+    console.log("toggling menu");
+}
